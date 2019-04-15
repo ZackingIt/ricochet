c7622a7926518690727c4449a0bc16bd8e4d1496
Zack Yang <zackyang@gmail.com>
2019-04-15 16:36:27 -0400

    added robot collison detection

--- utils/functionUtils.js
+++ utils/functionUtils.js
@@ -27,45 +27,49 @@ export const spliceRandomly = function(array) {
     return array.splice(Math.floor(Math.random()*(array.length-1)), 1)[0];
 };
 
-const traverseNorthSouth = (direction, { currentLoc, pieceList, board }) => {
+const traverse = (direction, { currentLoc, pieceList, board }) => {
     let [x, y] = currentLoc;
     if (direction === NORTH) {
-        while (!board[x][y].northWall && (y >= 1 && !board[x][y-1].southWall)) {
+        while (!board[x][y].northWall && (y >= 1 && !board[x][y - 1].southWall) && !robotCollision([x, y - 1], pieceList)) {
             y -= 1;
         }
     } else if (direction === SOUTH) {
-        while (!board[x][y].southWall && (y <= 15 && !board[x][y + 1].northWall)) {
+        while (!board[x][y].southWall && (y <= 15 && !board[x][y + 1].northWall) && !robotCollision([x, y + 1], pieceList)) {
             y += 1;
         }
-    }
-    return [x, y];
-};
-
-const traverseEastWest = (direction, { currentLoc, pieceList, board }) => {
-    let [x, y] = currentLoc;
-    if (direction === WEST) {
-        while (!board[x][y].westWall && (x >= 1 && !board[x - 1][y].eastWall)) {
+    } else if (direction === WEST) {
+        while (!board[x][y].westWall && (x >= 1 && !board[x - 1][y].eastWall) && !robotCollision([x - 1, y], pieceList)) {
             x -= 1;
         }
     } else if (direction === EAST) {
-        while (!board[x][y].eastWall && (x <= 15 && !board[x + 1][y].westWall)) {
+        while (!board[x][y].eastWall && (x <= 15 && !board[x + 1][y].westWall) && !robotCollision([x + 1, y], pieceList)) {
             x += 1;
         }
     }
     return [x, y];
 };
 
+const robotCollision = (location, pieceList) => {
+    let output = false;
+    pieceList.forEach((pieceLoc) => {
+        if (sameCoords(pieceLoc, location)){
+            output = true;
+        }
+    });
+    return output;
+};
+
 
 export const movePiece = function(direction, details) {
     switch(direction) {
         case NORTH:
-            return traverseNorthSouth(NORTH, details);
+            return traverse(NORTH, details);
         case SOUTH:
-            return traverseNorthSouth(SOUTH, details);
+            return traverse(SOUTH, details);
         case EAST:
-            return traverseEastWest(EAST, details);
+            return traverse(EAST, details);
         case WEST:
-            return traverseEastWest(WEST, details);
+            return traverse(WEST, details);
         default:
             return details.currentLoc;
         }
